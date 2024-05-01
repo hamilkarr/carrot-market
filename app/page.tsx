be0b8695c66ab6ea9a3f5757d7d3ce16d354ed49
hamilkarr <12uclock@gmail.com>
2024-05-01 21:59:40 +0900

    group modifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
     return (
         <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-            <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col  md:flex-row gap-3 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:bg-green-100">
-                <input
-                    className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
-                    type="text"
-                    required
-                    placeholder="Email address"
-                />
-                <span className='text-red-500 font-medium hidden peer-invalid:block'>Email is required</span>
-                <button className="bg-blue-500 text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 peer-invalid:bg-black">
-                    Login
-                </button>
+            <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col gap-4">
+                {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
+                    <div key={index} className="flex items-center gap-3 group">
+                        <div className="size-10 bg-blue-400 rounded-full"/>
+                        <span className="text-lg font-medium empty:w-24 empty:h-4 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">{person}</span>
+                        <div className="size-6 bg-red-500 text-white flex justify-center items-center rounded-full relative">
+                            <span className="z-10">{index}</span>
+                            <div className="size-6 bg-red-500 rounded-full absolute animate-ping"/>
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     );
