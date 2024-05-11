059e4ad97e7161b46c5c40882ae8abd0a094690d
hamilkarr <12uclock@gmail.com>
2024-05-11 15:52:49 +0900

    zod validation 설정 완료

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,52 @@
 'use server';
 import { z } from 'zod';
 
-const formSchema = z.object({
-    username: z.string().min(3).max(10),
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirmPassword: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes('potato');
+const checkPassword = ({
+    password,
+    confirmPassword,
+}: {
+    password: string;
+    confirmPassword: string;
+}) => password === confirmPassword;
+const passwordRegex = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
+const formSchema = z
+    .object({
+        username: z
+            .string({
+                invalid_type_error: 'Username must be a string',
+                required_error: 'Username is required',
+            })
+            .min(3, 'Way too short!!')
+            .trim()
+            .transform((username) => `🔥${username}🔥`)
+            .refine(checkUsername, 'custom error message'),
+        email: z
+            .string({
+                invalid_type_error: 'Email must be a string',
+                required_error: 'Email is required',
+            })
+            .trim()
+            .toLowerCase()
+            .email(
+                'Invalid email address. Please enter a valid email address.'
+            ),
+        password: z
+            .string()
+            .min(5)
+            .regex(
+                passwordRegex,
+                'A password must have lower case, UPPER CASE, number, and special character'
+            ),
+        confirmPassword: z.string().min(5),
+    })
+    .refine(checkPassword, {
+        message: "Passwords don't match",
+        path: ['confirmPassword'],
+    });
 
 export async function createAccount(prevState: any, formData: FormData) {
     const data = {
@@ -15,5 +55,11 @@ export async function createAccount(prevState: any, formData: FormData) {
         password: formData.get('password'),
         confirmPassword: formData.get('confirm-password'),
     };
-    formSchema.parse(data);
+    const result = formSchema.safeParse(data);
+    if (!result.success) {
+        console.log(result.error.flatten());
+        return result.error.flatten();
+    } else {
+        console.log(result.data);
+    }
 }
