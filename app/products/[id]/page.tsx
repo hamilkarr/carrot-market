cfd2e853ec216f924fa5aaa051d928640d1e416d
hamilkarr <12uclock@gmail.com>
2024-07-23 23:25:43 +0900

    상세페이지 완료, 외부 이미지 경로 추가

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,10 @@
 import db from '@/lib/db';
 import { getSession } from '@/lib/session';
 import { notFound } from 'next/navigation';
+import Image from 'next/image';
+import { UserIcon } from '@heroicons/react/24/solid';
+import { formatToWon } from '@/lib/utils';
+import Link from 'next/link';
 
 async function getIsOwner(userId: number) {
     const session = await getSession();
@@ -42,5 +46,34 @@ export default async function ProductDetail({
     }
     console.log(product);
     const isOwner = await getIsOwner(product.userId);
-    return <span>Product Detail of the product {id}</span>;
+    return (
+        <div>
+            <figure className="relative aspect-square">
+                <Image fill src={product.photo} alt={product.title} />
+            </figure>
+            <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+                <div className="size-10 rounded-full">
+                    {product.user.avatar !== null ? (
+                        <Image
+                            src={product.user.avatar}
+                            width={40}
+                            height={40}
+                            alt={product.user.username}
+                        />
+                    ) : (
+                        <UserIcon />
+                    )}
+                </div>
+                <h3>{product.user.username}</h3>
+            </div>
+            <div className='p-5'>
+                <h1 className='text-2xl font-semibold'>{product.title}</h1>
+                <p>{product.description}</p>
+            </div>
+            <div className='fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center'>
+                <span className='font-semibold text-lg'>{formatToWon(product.price)}원</span>
+                <Link href={''} className='bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold'>채팅하기</Link>
+            </div>
+        </div>
+    );
 }
