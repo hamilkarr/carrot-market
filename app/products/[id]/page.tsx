5a5acd44c7443c5fe66eb1242e2e9fc8416b78f1
hamilkarr <12uclock@gmail.com>
2024-07-23 23:07:21 +0900

    제품 상세페이지 개발중

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,46 @@
+import db from '@/lib/db';
+import { getSession } from '@/lib/session';
+import { notFound } from 'next/navigation';
+
+async function getIsOwner(userId: number) {
+    const session = await getSession();
+    if (session.id) {
+        return session.id === userId;
+    }
+    return false;
+}
+
+async function getProduct(id: number) {
+    const product = await db.product.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            user: {
+                select: {
+                    username: true,
+                    avatar: true,
+                },
+            },
+        },
+    });
+    return product;
+}
+
+export default async function ProductDetail({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const id = Number(params.id);
+    if (isNaN(id)) {
+        return notFound();
+    }
+    const product = await getProduct(id);
+    if (!product) {
+        return notFound();
+    }
+    console.log(product);
+    const isOwner = await getIsOwner(product.userId);
+    return <span>Product Detail of the product {id}</span>;
+}
--- components/list-product.tsx
+++ components/list-product.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { formatToTimeAgo, formatToWon } from "@/lib/utils";
 
 interface ListProductProps {
     title: string;
@@ -23,8 +24,8 @@ export default function ListProduct({
             </figure>
             <div className="flex flex-col gap-1 *:text-white">
                 <span className="text-lg" >{title}</span>
-                <span className="text-sm text-neutral-500" >{create_at.toString()}</span>
-                <span className="text-lg font-semibold" >{price}</span>
+                <span className="text-sm text-neutral-500" >{formatToTimeAgo(create_at.toString())}</span>
+                <span className="text-lg font-semibold" >{formatToWon(price)}원</span>
             </div>
         </Link>
     );
